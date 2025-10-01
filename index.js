32a06ffea0119896349937387ba0c36a9d13dc58
Charlie-emery <AA.charlie.emery@gmail.com>
2025-10-01 00:22:04 +0000

    inital comit - add calc distas func

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 (function(window, _) {
   window.jeuxBulb = window.jeuxBulb || {
-    numz: {},
+    numz: {
+        calculateDistance(p1, p2) {
+  var a = Math.abs(p1.x -p2.x)
+  var b = Math.abs(p1.y - p2.y)
+  var c = Math.pow(a, 2) + Math.pow(b, 2)
+  return Math.sqrt(c);
+}
+    },
     phyz: {},
   };
 }(window, window._));
